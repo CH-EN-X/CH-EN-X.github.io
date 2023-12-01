4f495364e588902e660a6846416c41fc67efb5d9
CH-EN-X <1211931367@qq.com>
2023-12-01 20:17:22 +0800

    Site updated: 2023-12-01 20:17:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/30/hello-world/"];function toRandomPost(){
+var posts=["2023/11/30/hello-world/","2023/11/27/MyLoverOpen/","2023/11/27/SpringBootWeb入门/","2023/11/27/Mybatis/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
