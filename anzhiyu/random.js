1fecf3027452c09e5a5db9a87ec6b6dbda4a7eb7
CH-EN-X <1211931367@qq.com>
2023-12-02 14:43:26 +0800

    Site updated: 2023-12-02 14:43:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/30/hello-world/","2023/11/27/MyLoverOpen/","2023/11/27/SpringBootWeb入门/","2023/11/27/Mybatis/"];function toRandomPost(){
+var posts=["2023/11/27/MyLoverOpen/","2023/11/27/Mybatis/","2023/12/02/hello-world/","2023/11/27/SpringBootWeb入门/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
